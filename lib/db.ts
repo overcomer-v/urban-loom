24e20d9006c27a458ec80d24b0ecc9d9483b7e7d
Victor Atoyeje <atoyejeovercomer2@gmail.com>
2026-09-03 00:01:53 +0100

    Migration to Neon-Vercel-DB

--- lib/db.ts
+++ lib/db.ts
@@ -1,8 +1,8 @@
 import { Pool } from 'pg';
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE2_URL,
-  ssl: process.env.DATABASE2_URL?.includes("localhost") ? false : { rejectUnauthorized: false },
+  connectionString: process.env.DATABASE2_DATABASE_URL,
+  ssl: process.env.DATABASE2_DATABASE_URL?.includes("localhost") ? false : { rejectUnauthorized: false },
 });
 
 export default pool;
