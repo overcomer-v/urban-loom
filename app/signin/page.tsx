abf1571793da1a419319375d5ab414dee3f0a03f
Victor Atoyeje <atoyejeovercomer2@gmail.com>
2026-09-18 14:21:37 +0100

    fix:UI Adaptations II

--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -1,9 +1,13 @@
 import LoginForm from "@/components/auth/LogInForm";
+import { Loader } from "lucide-react";
+import { Suspense } from "react";
 
 export default async function LoginPage() {
-    return(
-        <div className="m-auto py-32">
-              <LoginForm/>
-        </div>
-    );
+  return (
+    <div className="m-auto py-32">
+      <Suspense fallback={<Loader className="animate-spin" />}>
+        <LoginForm />
+      </Suspense>
+    </div>
+  );
 }
